6a7cd5189b11e1b173616b04cdf6655410d9fcb1
Jun Seol <physiogel@pusan.ac.kr>
2022-06-18 11:57:35 +0900

    feat: websocket 기본 controller 작성

---
+++ src/controllers/websocket.ts
@@ -0,0 +1,64 @@
+// import Offer from 'models/offer';
+// import Answer from 'models/answer';
+// import Candidate from 'models/candidate';
+
+const clients: Map<WebSocket, Set<string>> = new Map<WebSocket, Set<string>>();
+const connectionPair: Map<string, [WebSocket, WebSocket]> = new Map<string, [WebSocket, WebSocket]>();
+
+const getOrCreateConnectionIds = (session: WebSocket): Set<string> => {
+    let connectionIds = null;
+
+    if (!clients.has(session)) {
+        connectionIds = new Set<string>();
+        clients.set(session, connectionIds);
+    }
+    connectionIds = clients.get(session);
+
+    return connectionIds;
+};
+
+const add = (ws: WebSocket): void => {
+    clients.set(ws, new Set<string>());
+};
+
+const remove = (ws: WebSocket): void => {
+    const connectionIds = clients.get(ws);
+
+    connectionIds.forEach((connectionId) => {
+        const pair = connectionPair.get(connectionId);
+
+        if (pair) {
+            const otherSessionWs = pair[0] == ws ? pair[1] : pair[0];
+
+            if (otherSessionWs) {
+                otherSessionWs.send(JSON.stringify({ type: 'disconnect', connectionId: connectionId }));
+            }
+        }
+    });
+
+    clients.delete(ws);
+};
+
+const onConnect = (ws: WebSocket, connectionId: string): void => {
+    const connectionIds = getOrCreateConnectionIds(ws);
+    connectionIds.add(connectionId);
+
+    ws.send(JSON.stringify({ type: 'connect', connectionId: connectionId, polite: true }));
+};
+
+const onDisconnect = (ws: WebSocket, connectionId: string): void => {
+    const connectionIds = clients.get(ws);
+    connectionIds.delete(connectionId);
+
+    if (connectionPair.has(connectionId)) {
+        const pair = connectionPair.get(connectionId);
+        const otherSessionWs = pair[0] == ws ? pair[1] : pair[0];
+
+        if (otherSessionWs) {
+            otherSessionWs.send(JSON.stringify({ type: 'disconnect', connectionId: connectionId }));
+        }
+    }
+    connectionPair.delete(connectionId);
+
+    ws.send(JSON.stringify({ type: 'disconnect', connectionId: connectionId }));
+};
