80c2e5b7e24b38c8bd76e032c40d668f78726026
Jun Seol <physiogel@pusan.ac.kr>
2022-07-08 09:42:10 +0900

    feat: signaling 라우터 추가

--- src/router/signaling.ts
+++ src/router/signaling.ts
@@ -5,4 +5,20 @@ const router: express.Router = express.Router();
 
 router.use(http.checkSessionId);
 
+router.get('', http.getAllConnections);
+router.get('/connection', http.getConnection);
+router.get('/offer', http.getOffer);
+router.get('/answer', http.getAnswer);
+router.get('/candidate', http.getCandidate);
+
+router.post('/offer', http.postOffer);
+router.post('/answer', http.postAnswer);
+router.post('/candidate', http.postCandidate);
+
+router.put('', http.createSession);
+router.put('/connection', http.createConnection);
+
+router.delete('', http.deleteSession);
+router.delete('/connection', http.deleteConnection);
+
 export default router;
