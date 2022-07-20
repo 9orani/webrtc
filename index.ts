13fa5a7be67cd9076f4a9392a2c14c5c3dd4d77e
Jun Seol <physiogel@pusan.ac.kr>
2022-07-20 14:40:25 +0900

    fix: port env화

--- index.ts
+++ index.ts
@@ -3,8 +3,10 @@ import { createServer } from './src/server';
 import * as express from 'express';
 import WSSignaling from './src/websocket';
 
+const port = process.env.WEBRTC_PORT;
+
 const app: express.Application = createServer();
-const server: Server = app.listen(8081);
+const server: Server = app.listen(port);
 
 new WSSignaling(server);
-console.log('start websocket signaling server 8081');
+console.log(`start websocket signaling server ${port}`);
